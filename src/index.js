9c595037ff291585de67ccfcefd99010d8a802d3
AbdurahmonQobiljonov <87411075+AbdurahmonQobiljonov@users.noreply.github.com>
2021-08-08 12:03:35 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class='root'>
+<h1>Abduraxmon Qobiljonov</h1>
+<h2>Year of Graduation:2023</h2>
+<b>Active Email:</b>
+<a href='https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox'>
+Xonobod.n@gmail.com
+</a>
+<p><b>Phon namber :</b>    +998 99 611 57 31 </p>
+ <p>previous work experianse  with time:</p> 
+ <ol>
+   <li> I started writing codes from March to 2021</li>
+   <li>I am learning React with my team 2021</li>
+
+ </ol>
+</div>
+`;
